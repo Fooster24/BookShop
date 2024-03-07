1a5dc69cb0af3a7169b4cf12c9650866bc839306
fooster24 <fooster2461@gmail.com>
2024-03-07 13:29:25 -0400

    crud operation

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {PORT,MongoDBURL} from './config.js'
-import { MongoClient, ServerApiVersion } from "mongodb"
+import { MongoClient, ServerApiVersion, ObjectId } from "mongodb"
 const app = express()
 
 app.use(express.json())
@@ -12,30 +12,49 @@ const client = new MongoClient(MongoDBURL,  {
         deprecationErrors: true,
     }
 });
-    const booksDB = client.db("my BookShop")
+    const booksDB = client.db("myBookShop")
     const mybooks = booksDB.collection("booksCollection")
 
 app.listen(PORT,()=> {
-    console.log('Server started on port ${PORT 3000}')
+    console.log(`Server started on port ${PORT}`)
 })
 
-app.get('/',(reg, res)=> {
+app.get('/',(req, res)=> {
    return res.status(200).send("<ch1>Yoo Dawg!/<ch1>")
 
 })
 
-    app.get('/shop',(reg, res)=> {
-    return    res.status(232).send("Welcome to booty claping basic")
+    app.get('/shop',(req, res)=> {
+    //return    res.status(232).send("Welcome to booty claping basic")
+    mybooks.find().toArray()
+    .then(response=>{
+        //console.log(response)
+        res.status(200).send(response)
+    })
     
 })
 
-app.get('/shop/:id', (reg, res)=> {
-    const data = reg.params
-    return res.status(232).send(232).send (`<a herf ='/'> Book: ${data.id}</a>`)
+app.get('/shop/:id', (req, res)=> {
+    //route ahow specific book
+    const data = req.params
+
+    const filter = {
+        "_id": new ObjectId(data.id)
+    }
+
+    mybooks.findOne()
+    .then(response=>{
+        //console.log(response)
+        res.status(200).send(response)
+
+    })
+    .catch(err=>console.log(err))
+    //return res.status(232).send(232).send (`<a herf ='/'> Book: ${data.id}</a>`)
 
 })
 
-app.post('/savebook',(req, res)=> {
+app.post('/admin/savebook',(req, res)=> {
+    // route adds a new book
     const data = req.body
     if (!data.title)
     return res.status(400).send("No title found.")
@@ -56,3 +75,40 @@ app.post('/savebook',(req, res)=> {
     return res.status(201).send(JSON.stringify(data))
 
 })
+
+app.delete('/admin/remove/:id', (req, res)=>{
+    const data = req.params
+    
+    const filter = {
+        "_id": new ObjectId(data.id)
+    }
+
+    mybooks.deleteOne(filter)
+    .then(response=>{
+        //console.log(response)
+        res.status(200).send(response)
+
+    })
+    .catch(err=>console.log(err))
+})
+
+app.put('/admin/update/:id/', (req,res)=>{
+    const docData = req.body
+    const data = req.params
+    const filter = {
+        "_id": new ObjectId(data.id)
+    }
+    
+    const updDoc = {
+        $set: {
+            ...docData //docData.price, docData.cover
+            
+        }
+    }
+    mybooks.updateOne(filter, updDoc)
+    .then(response=>{
+        res.status(200).send(response)  
+     })
+     .catch(err=>console.log(err))
+})
+
